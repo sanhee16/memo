491d2bae2c19c2f9ca5b1fbf8d395e4ca5638de6
DESKTOP-8K33LU6\USER <sinhioa20@naver.com>
2021-06-21 21:28:09 +0900

    fragment 선언
    - memoList와 memoInfo fragment 2개 선언

--- app/src/main/java/com/damtoto/MainActivity.kt
+++ app/src/main/java/com/damtoto/MainActivity.kt
@@ -2,10 +2,22 @@ package com.damtoto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.damtoto.databinding.ActivityMainBinding
 
+lateinit var binding : ActivityMainBinding
+
+// https://juahnpop.tistory.com/227
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val memoList = supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayoutA, memoList())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/damtoto/memoInfo.kt
@@ -0,0 +1,70 @@
+package com.damtoto
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [memoInfo.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class memoInfo : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    var mainActivity: MainActivity? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_memo_info, container, false)
+//        binding.btnShowFragmentB.setOnClickListener {
+//            mainActivity!!.openFragmentOnFrameLayoutB(1)
+//        }
+        return binding.root
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment memoInfo.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            memoInfo().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
